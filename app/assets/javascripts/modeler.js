a71cd6de611f503adf71ca6742dfa7a5a93a1585
alexbispo <alexbispo.artes@gmail.com>
2019-01-03 23:36:15 +0000

    Exibe novo diagrama para edição.

---
+++ app/assets/javascripts/modeler.js
@@ -0,0 +1,62 @@
+var diagramUrl = '/diagrams/newDiagram.bpmn';
+
+//var diagramUrl = 'https://cdn.rawgit.com/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+// modeler instance
+var bpmnModeler = new BpmnJS({
+  container: '#canvas',
+  keyboard: {
+    bindTo: window
+  }
+});
+
+function createNewDiagram() {
+  openDiagram(newDiagramXML);
+}
+
+/**
+  * Save diagram contents and print them to the console.
+  */
+function exportDiagram() {
+  bpmnModeler.saveXML({ format: true }, function(err, xml) {
+    if (err) {
+      return console.error('could not save BPMN 2.0 diagram', err);
+    }
+    alert('Diagram exported. Check the developer tools!');
+    console.log('DIAGRAM', xml);
+  });
+}
+/**
+  * Open diagram in our modeler instance.
+  *
+  * @param {String} bpmnXML diagram to display
+  */
+function openDiagram(bpmnXML) {
+  // import diagram
+  bpmnModeler.importXML(bpmnXML, function(err) {
+    if (err) {
+      return console.error('could not import BPMN 2.0 diagram', err);
+    }
+    // access modeler components
+    /*
+    var canvas = bpmnModeler.get('canvas');
+    var overlays = bpmnModeler.get('overlays');
+    // zoom to fit full viewport
+    canvas.zoom('fit-viewport');
+    // attach an overlay to a node
+    overlays.add('SCAN_OK', 'note', {
+      position: {
+        bottom: 0,
+        right: 0
+      },
+      html: '<div class="diagram-note">Mixed up the labels?</div>'
+    });
+    // add marker
+    canvas.addMarker('SCAN_OK', 'needs-discussion');
+    */
+  });
+}
+// load external diagram file via AJAX and open it
+$.get(diagramUrl, openDiagram, 'text');
+// wire save button
+//createNewDiagram();
+$('#save-button').click(exportDiagram);
